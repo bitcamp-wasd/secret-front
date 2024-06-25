ab649477f97894fe5d8057f377b9f4fcd6143d56
KimYung1994 <157556953+KimYung1994@users.noreply.github.com>
2024-06-25 16:10:57 +0900

    메인페이지 화면 구현 및 배너 수정

---
+++ src/pages/Main/MainPage.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import "../../assets/css/style.css";
+import "../../components/Layout";
+import Button from "../../components/Button";
+import Layout from "../../components/Layout";
+import Tag from "../../components/Tag";
+import VideoBox from "../../components/VideoBox";
+import Upload from "../../assets/images/upload.svg";
+import Search from "../../assets/images/search.svg"
+
+const MainPage = () => {
+  // 더미 데이터 생성
+  const dummyVideos = Array.from({ length: 12 }, (_, index) => ({
+    id: index + 1,
+    title: `Video ${
+      index + 1
+    } 이렇게 제목이 길면 너가 뭘 할 수 있는지 궁금한데`,
+    thumbnail: `https://via.placeholder.com/276x155.25?text=Thumbnail+${
+      index + 1
+    }`,
+    author: "홍길동",
+  }));
+
+  const handleSearch = (event) => {
+    if (event.key === "Enter") {
+      // 검색 이벤트 처리
+      console.log("검색어:", event.target.value);
+    }
+  };
+
+  return (
+    <Layout showFooter={false}>
+      <div className="main-container-1150 mt80">
+        <div class="row-direction space-between mb50">
+        
+        
+          <Tag />
+          
+          <div className="tag-main-right">
+            <Button size="tag">
+              <span className="icon-wrapper">
+                <img src={Upload} alt="upload" />
+              </span>
+              동영상 업로드
+            </Button>
+
+            
+              <input
+                type="text"
+                placeholder="검색"
+                onKeyDown={handleSearch}
+                className="search-input"
+              />
+              
+            
+          </div>
+        </div>
+        <div className="videos-grid">
+          {dummyVideos.map((video) => (
+            <VideoBox
+              key={video.id}
+              thumbnail={video.thumbnail}
+              title={video.title}
+              author={video.author}
+            />
+          ))}
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default MainPage;
