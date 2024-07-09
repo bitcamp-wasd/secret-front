266ebccd4644621830a8f1544003388622e17091
KimYung1994 <157556953+KimYung1994@users.noreply.github.com>
2024-07-09 12:17:41 +0900

    마이페이지-내동영상 연결완료 무한스크롤을 intersection observer로 바꿈 videobox컴포넌트 데이터 받을 수 있게 수정 nickName에서 nickname으로 수정

--- src/components/VideoBox.jsx
+++ src/components/VideoBox.jsx
@@ -1,26 +1,36 @@
 import React from "react";
 import "../assets/css/style.css";
 
-const VideoBox = ({ thumbnail, title }) => {
+
+const defaultThumbnail = "https://via.placeholder.com/276x155.25?text=Thumbnail";
+
+const VideoBox = ({ thumbnail, title, views, uploadDate, length, nickname }) => {
+  // 동영상 길이를 "MM:SS" 형식으로 변환
+  const formatLength = (length) => {
+    const minutes = Math.floor(length / 60);
+    const seconds = length % 60;
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  };
+
   return (
     <div className="video-box">
-      <img className="video-box-thumbnail" src={thumbnail} alt={title} />
-
-      <div class="row-direction space-between pdx30">
-        <div class="movie-small">
+      {/* 아직 이미지 불러오기 구현안돼서 디폴트 썸네일 사용 */}
+      <img className="video-box-thumbnail" src={defaultThumbnail ||thumbnail } alt="임시" />
+      <div className="row-direction space-between pdx30">
+        <div className="movie-small">
           <span>{title}</span>
-          <ul class="movie-small-list">
+          <ul className="movie-small-list">
             <li>
-              <span>조회수 2536</span>
+              <span>조회수 {views}</span>
             </li>
             <li>
-              <span>&#8226; 2일전</span>
+              <span>&#8226; {uploadDate}</span>
             </li>
             <li>
-              <span>&#8226; 19:36</span>
+              <span>&#8226; {formatLength(length)}</span>
             </li>
             <li>
-              <span>김융</span>
+              <span>{nickname}</span>
             </li>
           </ul>
         </div>
