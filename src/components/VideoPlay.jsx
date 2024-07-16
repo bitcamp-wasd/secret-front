2e5e9ad10407811bc0ec67cea75315295e5293b2
KimYung1994 <157556953+KimYung1994@users.noreply.github.com>
2024-07-16 11:38:54 +0900

    동영상 재생 클라우드와 연결

--- src/components/VideoPlay.jsx
+++ src/components/VideoPlay.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "../assets/css/style.css";
 
-const VideoView = ({ thumbnail, title }) => {
-    return (
-        <div className="video-play">
-            <img src={thumbnail} alt={title} />
-            <p>{title}</p>
-        </div>
-    );
-};
+const VideoPlay = ({ thumbnail, videoUrl }) => {
+    useEffect(() => {
+      if (videoUrl) {
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = 'https://player.vpe.naverncp.com/ncplayer.1.1.2.js?access_key=3ba6f6a18046b72c8fcd9f59aca5f5d5';
+        script.async = true;
+        console.log(videoUrl);
+        script.onload = () => {
+          const player = new window.ncplayer('video', {
+            playlist: [
+              {
+                file: videoUrl,
+                
+              },
+            ],
+            autostart: true,
+          });
+        };
+        document.getElementById('video').appendChild(script);
+      }
+    }, [videoUrl, thumbnail]);
+  
+    return <>
 
-export default VideoView;
+    <div id="video">
+    </div>
+
+    </>;
+  };
+  
+
+export default VideoPlay;
